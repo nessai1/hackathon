03c33645c880e48ab86c4176ede90b0f77ffc9a8
Daniil Verevkin <werowkin@gmail.com>
2021-07-03 11:40:34 +0200

    страница логина

---
+++ resources/js/pages/LoginPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form action="#" class="align-baseline ">
+            <div class="form-row">
+                <input type="text" v-model="formData.login">
+            </div>
+            <div class="form-row">
+                <input type="password" v-model="formData.password">
+            </div>
+            <div class="form-row">
+                <button type="submit">Sign In</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            formData: {
+                password: '',
+                login: '',
+            },
+        }
+    }
+}
+</script>
